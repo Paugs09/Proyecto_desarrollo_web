9b4953ed4b8f6a7e20826dd6b1a05de9293d2253
paugs09 <mariagomez@colegioguanenta.edu.co>
2026-03-02 17:33:40 -0500

    Mis cambios antes de hacer pull

--- Frontend/hormi-ruta/src/app/app.component.ts
+++ Frontend/hormi-ruta/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
